5becc7c757b417814088eca8d9777d7f3ba8c429
kkmed <krithimeda1652@gmail.com>
2026-05-02 23:58:07 +0530

    Made Changes To This File

--- student_logic.py
+++ student_logic.py
@@ -6,7 +6,7 @@ draw = ImageDraw.Draw(img)
 
 # 2. Load the font you copied (change "Arial.ttf" if yours is different)
 # The number 80 is the font size. Make it bigger or smaller as needed.
-font = ImageFont.truetype("Arial.ttf", 80)
+font = ImageFont.truetype("fonts/DejaVuSans.ttf", 80)
 
 # 3. Add the text
 # Use the X and Y coordinates you found in Step 1
