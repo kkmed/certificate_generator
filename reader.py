7d73543b7a3116080cab3e1623da23b7bef201f8
Shloka Konakalla <shlokakonakalla01@gmail.com>
2026-05-08 15:17:48 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,9 +1,23 @@
-import pandas as pd
+import json
 
-def read_csv_file(file_path):
+
+def read_manual_input():
+    return {
+        "organization_name": input("Organization Name: "),
+        "organization_tagline": input("Tagline: "),
+        "certificate_title": input("Certificate Title: "),
+        "subtitle": input("Subtitle: "),
+        "recipient_name": input("Recipient Name: "),
+        "body_line": input("Body Line: "),
+        "event_name": input("Event Name: "),
+        "organized_by": input("Organized By: ")
+    }
+
+
+def read_json_file(file_path):
     try:
-        df = pd.read_csv(file_path)
-        return df.to_dict(orient="records")
+        with open(file_path, "r") as file:
+            return json.load(file)
     except Exception as e:
-        print("Error reading file:", e)
-        return []
+        print("Error reading JSON:", e)
+        return {}
